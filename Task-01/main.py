fae90ead72dd4547b159193e87bd387a74b328bb
nandhu-44 <nandhu90040@gmail.com>
2024-05-07 11:16:56 +0530

    Completed Task 1

---
+++ Task-01/main.py
@@ -0,0 +1,11 @@
+"""
+Task 1
+Declare a variable in Python.
+"""
+
+var_int: int = 10
+var_float: float = 3.14
+var_string: str = "Hello, World!"
+var_bool: bool = True
+
+print(var_int, var_float, var_string, var_bool, sep="\t")
